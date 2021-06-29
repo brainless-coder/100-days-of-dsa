544ef568dd340d056bbf7fb5467e346d098c1b21
Arvind Prime <singhamish27@gmail.com>
2021-06-29 22:15:59 +0530

    day 4: solved 2 questions of hashing

--- hashing.py
+++ hashing.py
@@ -12,17 +12,86 @@ def maxmFreq(arr):
         if (freq[key] > maxmVal):
             maxmKey = key
             maxmVal = freq[key]
-
     print(maxmKey)
 
+# O(n^2)
+def pairSumTo0Naive(arr):
+    count = 0
+    for i in range(len(arr)):
+        if arr[i] < 0:
+            for j in range(len(arr)):
+                if i != j:
+                    if arr[i] == -arr[j]:
+                        print(arr[i], arr[j])
+                        count += 1
+    print("Count: " + str(count))
+
+# O(n) with 2 loops
 def pairSumTo0(arr):
-    neg = []
-    for ele in arr:
-        if ele < 0:
-            neg.append(ele)
+    freq = {}
+    count = 0
+    for i in range(len(arr)):
+        if arr[i] in freq:
+            freq[arr[i]] += 1
+        else:
+            freq[arr[i]] = 1
     
-    for ele in neg:
-        pass
+    for key in freq:
+        if key < 0:
+            if -key in freq:
+                for i in range(freq[key]*freq[-key]):
+                    print(key, -key)
+                count += (freq[key]*freq[-key])
+    print(count)
+
+# O(n) with one loop
+def pairSumTo0Optimized(arr):
+    freq = {}
+    count = 0
+    for ele in arr:
+        if -ele in freq:
+            for i in range(freq[-ele]):
+                print(ele, -ele)
+            count += freq[-ele]
+        if ele in freq:
+            freq[ele] += 1
+        else:
+            freq[ele] = 1
+    print(count)
+
+def extractUniqueChar(s):
+    freq = {}
+    ans = ""
+    for char in s:
+        if char not in freq:
+            freq[char] = 1
+            ans += char
+    print(ans)
+
+def longestConsSeq(arr):
+    arr.sort()
+    freq = {}
+    for ele in arr:
+        if ele in freq:
+            freq[ele] += 1
+        else:
+            freq[ele] = 1
+    print(freq)
+
+    for key in freq:
+        if key+1 in freq:
+            print(key, end=" ")
+        else:
+            print(key)
+    
+        
+
 
 arr = [int(x) for x in input().split()]
-maxmFreq(arr)
+# maxmFreq(arr)
+# pairSumTo0Naive(arr)
+# pairSumTo0(arr)
+# pairSumTo0Optimized(arr)
+# s = input()
+# extractUniqueChar(s)
+longestConsSeq(arr)
