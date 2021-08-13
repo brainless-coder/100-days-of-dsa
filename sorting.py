7701b77a30c28e1e354947f281ac2c8eda2415cf
Arvind Prime <singhamish27@gmail.com>
2021-08-13 23:49:06 +0530

    Day 20: implemented quick sort and merge sort

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+
 # O(n^2)
 def bubbleSort(arr):
     n = len(arr)
@@ -36,17 +37,18 @@ def insertionSort(arr):
         i += 1
     print(arr)
 
-
+# O(nlong)
 def mergeSort(arr):
-    if len(arr) == 0:
-        return
+    if len(arr) == 0 or len(arr) == 1:
+        return arr
     mid = len(arr)//2
     li1 = mergeSort(arr[:mid])
     li2 = mergeSort(arr[mid:])
-    arr = merge(li1, li2)
+    arr = merge(li1, li2, arr)
+    return arr
 
 # O(n+m)
-def merge(li1, li2):
+def merge(li1, li2, arr):
     n, m = len(li1), len(li2)
     arr = [0 for i in range(n+m)]
     i, j, k = 0, 0, 0
@@ -69,14 +71,61 @@ def merge(li1, li2):
         j += 1
         k += 1
     
-    print(arr)
+    return arr
+    
+
+def quickSort(arr, start, end):
+    if start >= end:
+        return
+    pivot = partition(arr, start, end)
+    quickSort(arr, start, pivot-1)
+    quickSort(arr, pivot+1, end)
+
+def partition(arr, start, end):
+    pivot = start
+    i = start
+    count = 0
+    while i <= end:
+        if arr[i] < arr[pivot]:
+            count += 1
+        i += 1
+    arr[pivot], arr[start+count] = arr[start+count], arr[pivot]
+    pivot = start+count
+    i, j = start, end
+    while i < j:
+        if arr[i] > arr[pivot]:
+            arr[i], arr[j] = arr[j], arr[i]
+            j -= 1
+        else:
+            i += 1
+    return pivot
 
 
-
-# arr = [int(x) for x in input().split()]
+arr = [int(x) for x in input().split()]
+n = len(arr)
 # bubbleSort(arr)
 # selectionSort(arr)
 # insertionSort(arr)
-li1 = [int(x) for x in input().split()]
-li2 = [int(x) for x in input().split()]
-merge(li1, li2)
+# print(mergeSort(arr))
+quickSort(arr, 0, n-1)
+print(arr)
+
+
+
+def binarySearch(arr, x):
+    n = len(arr)
+    start, end = 0, n-1
+    
+    for i in range(n):
+        mid = (start + end) // 2
+        if x > arr[i]:
+            start = mid + 1
+        elif x < arr[i]:
+            end = mid - 1
+        else:
+            return True
+    return False
+
+# print(binarySearch([1, 2, 5, 8, 12, 15, 18], 1))
+
+
