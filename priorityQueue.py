a85a2e2ee5b14d427b39a6f3fa1e7e7546816262
Arvind Prime <singhamish27@gmail.com>
2021-11-05 20:12:52 +0530

    Day 35: min PQ remove function

--- priorityQueue.py
+++ priorityQueue.py
@@ -36,25 +36,33 @@ class minPriorityQueue:
 
     def __percolateDown(self):
         parentIndex = 0
-        while True:
-            leftChild, rightChild = 2*parentIndex+1, 2*parentIndex+2
-            if rightChild < self.getSize():
-                if self.pq[parentIndex].priority > self.pq[leftChild].priority and self.pq[leftChild].priority < self.pq[rightChild].priority:
-                    self.pq[parentIndex], self.pq[leftChild] = self.pq[leftChild], self.pq[parentIndex]
-                    parentIndex = leftChild
-                elif self.pq[parentIndex].priority > self.pq[rightChild].priority and self.pq[rightChild].priority < self.pq[leftChild].priority:
-                    self.pq[parentIndex], self.pq[rightChild] = self.pq[rightChild], self.pq[parentIndex]
-                    parentIndex = rightChild
-                else:
-                    break
-            elif leftChild < self.getSize():
-                if self.pq[parentIndex].priority > self.pq[leftChild].priority:
-                    self.pq[parentIndex], self.pq[leftChild] = self.pq[leftChild], self.pq[parentIndex]
+        leftChild, rightChild = 2*parentIndex+1, 2*parentIndex+2
+        while leftChild < self.getSize():
+            minIdx = parentIndex
+            if self.pq[leftChild].priority < self.pq[minIdx].priority:
+                minIdx = leftChild
+            if rightChild < self.getSize() and self.pq[rightChild].priority < self.pq[minIdx].priority :
+                minIdx = rightChild
+            if parentIndex == minIdx:
                 break
+            self.pq[parentIndex], self.pq[minIdx] = self.pq[minIdx], self.pq[parentIndex]
+            parentIndex = minIdx
+            leftChild, rightChild = 2*parentIndex+1, 2*parentIndex+2
 
     def removeMin(self):
+        if self.isEmpty():
+            return None
         x = self.pq[0].value
         self.pq[0] = self.pq[self.getSize()-1]
         self.pq.pop()
         self.__percolateDown()
         return x
+
+minPQ = minPriorityQueue()
+minPQ.insert('A', 10)
+minPQ.insert('C', 5)
+minPQ.insert('B', 19)
+minPQ.insert('D', 4)
+print(minPQ.getMin())
+for i in range(4):
+    print(minPQ.removeMin())
